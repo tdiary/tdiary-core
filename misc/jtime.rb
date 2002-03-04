539d980bf86406d128e2e006a9a3d305470b74a3
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-03-04 13:47:26 +0000

    add discription in header.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@86 7f22e88f-374d-0410-998f-c91420d97ba2

--- misc/jtime.rb
+++ misc/jtime.rb
@@ -2,6 +2,11 @@
 # 「%J」で日本語の曜日名を出すTimeクラス改造版
 # tdiary.conf内でrequireして使う
 #
+# 例: require 'jtime'
+#
+# 環境によってはjtime.rbの置き場所を選ぶ場合あり。
+# /usr/local/lib/ruby/site_ruby(など)に入れると吉。
+#
 class Time
    alias strftime_ strftime
    JWDAY = %w(日 月 火 水 木 金 土)
