db50df4f84076cf61c4dea71e177e9d9ee743e94
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-04-19 06:52:27 +0000

    fix bug of paragraph number.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@381 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,12 +1,12 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.25 $
+tdiary.rb $Revision: 1.26 $
 
 Copyright (C) 2001-2002, TADA Tadashi <sho@spc.gr.jp>
 =end
 
-TDIARY_VERSION = '1.4.1.20020418'
+TDIARY_VERSION = '1.4.1.20020419'
 
 require 'cgi'
 require 'nkf'
