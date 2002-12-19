b78a2d64933c7ab6735e5ea74ee6bf8f60c71c31
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-12-19 04:13:01 +0000

    * 00default.rb: fix nyear title. thank uechi <uh@u.dhis.portside.net>.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@971 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/00default.rb
+++ plugin/00default.rb
@@ -1,6 +1,6 @@
 #
 # 00default.rb: default plugins 
-# $Revision: 1.23 $
+# $Revision: 1.24 $
 #
 
 #
@@ -204,7 +204,7 @@ def title_tag
 		r << '(ÀßÄê´°Î»)'
 	when 'nyear'
 		years = @diaries.keys.map {|ymd| ymd.sub(/^\d{4}/, "")}
-		r << "(#{years[0].sub( /^(\d\d)/, '\1-')}[#{nyear_diary_label @date, years}])" if @date
+		r << "(#{@cgi['date'][0].sub( /^(\d\d)/, '\1-')}[#{nyear_diary_label @date, years}])" if @date
 	end
 	r << '</title>'
 end
