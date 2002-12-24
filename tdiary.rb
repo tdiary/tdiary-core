17a4098d3fc7f0999e4ac2209f7eaa55651b61b0
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2002-12-24 02:19:09 +0000

    * skel/i.*.rhtml: support @hide_comment_form in tdiary.conf.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@986 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,12 +1,12 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.76 $
+tdiary.rb $Revision: 1.77 $
 
 Copyright (C) 2001-2002, TADA Tadashi <sho@spc.gr.jp>
 =end
 
-TDIARY_VERSION = '1.5.1.20021218'
+TDIARY_VERSION = '1.5.1.20021224'
 
 require 'cgi'
 require 'nkf'
