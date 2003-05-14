0256b121c7371272ff9eee3ecda40c03d49d9f7a
uechi <uechi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-05-14 23:26:19 +0000

    * rd_style.rb: fix footnote element
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@1396 7f22e88f-374d-0410-998f-c91420d97ba2

--- misc/style/rd/rd_style.rb
+++ misc/style/rd/rd_style.rb
@@ -1,5 +1,5 @@
 #
-# rd_style.rb: RD style for tDiary 2.x format. $Revision: 1.9 $
+# rd_style.rb: RD style for tDiary 2.x format. $Revision: 1.10 $
 # based on Wiki style which Copyright belongs to TADA Tadashi.
 #
 # if you want to use this style, install RDtool
@@ -103,7 +103,8 @@ module RD
 		end
 
 		def apply_to_Footnote(element, content)
-			%Q|<%=fn "#{content}"%>|
+			heredoc_id = "%0.32b" % rand( 0x100000000 )
+			%Q|<%=fn <<'#{heredoc_id}' \n #{content.join}\n#{heredoc_id}\n%>|
 		end
 
 		def apply_to_RefToElement(element, content)
