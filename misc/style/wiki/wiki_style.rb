43e780bcd9240e4614ceb3005816de38b65c25e5
kitaj <kitaj@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-06-07 06:58:56 +0000

    * wiki_style.rb: remove categorizable?
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@1430 7f22e88f-374d-0410-998f-c91420d97ba2

--- misc/style/wiki/wiki_style.rb
+++ misc/style/wiki/wiki_style.rb
@@ -1,5 +1,5 @@
 #
-# wiki_style.rb: WikiWiki style for tDiary 2.x format. $Revision: 1.11 $
+# wiki_style.rb: WikiWiki style for tDiary 2.x format. $Revision: 1.12 $
 #
 # if you want to use this style, add @style into tdiary.conf below:
 #
@@ -422,10 +422,6 @@ module TDiary
 	
 		def to_s
 			"date=#{date.strftime('%Y%m%d')}, title=#{title}, body=[#{@sections.join('][')}]"
-		end
-
-		def categorizable?
-			true
 		end
 	end
 end
