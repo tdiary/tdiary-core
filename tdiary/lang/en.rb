6d087eb2abb820b22c29f12e0e703cffe2cc013e
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-09-26 08:52:30 +0000

    * tdiary/lang/en.rb: add comments.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@1567 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary/lang/en.rb
+++ tdiary/lang/en.rb
@@ -1,33 +1,56 @@
 #
-# tDiary language setup: English(en)
+# tDiary language setup: English(en) $Revision: 1.2 $
 #
 
+#
+# setting ruby charctor encoding.
+# 
 $KCODE = 'n'
 
+#
+# 'html_lang' method returns String of HTML language attribute.
+# 
 def html_lang
 	'en-US'
 end
 
+#
+# 'encoding' method returns String of HTTP or HTML charactor encoding.
+# 
 def encoding
 	'ISO-8859-1'
 end
 
+#
+# 'mobile_encoding' method returns charactor encoding in mobile mode.
 def mobile_encoding
 	'ISO-8859-1'
 end
 
+#
+# 'to_native' method converts string automatically to native encoding.
+# 
 def to_native( str )
 	str.dup
 end
 
+#
+# 'to_mobile' method converts string automatically to mobile mode encoding.
+# 
 def to_mobile( str )
 	str.dup
 end
 
+#
+# 'to_mail' method converts string automatically to E-mail encoding.
+# 
 def to_mail( str )
 	str.dup
 end
 
+#
+# 'shorten' method cuts string length.
+# 
 def shorten( str, length = 120 )
 	matched = str.gsub( /\n/, ' ' ).scan( /^.{0,#{length - 2}}/ )[0]
 	unless $'.empty?
@@ -37,6 +60,9 @@ def shorten( str, length = 120 )
 	end
 end
 
+#
+# 'comment_length' returns length of shorten comment on recent or monthly view.
+#
 def comment_length
 	120
 end
