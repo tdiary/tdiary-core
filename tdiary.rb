f1a46aa2fd870f37c0209d15db4b274acd61fea9
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-11-10 09:10:56 +0000

    * skel/update.rhtml*: add subtitle in update form.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@1664 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,13 +1,13 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.154 $
+tdiary.rb $Revision: 1.155 $
 
 Copyright (C) 2001-2003, TADA Tadashi <sho@spc.gr.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '1.5.5.20031029'
+TDIARY_VERSION = '1.5.5.20031110'
 
 require 'cgi'
 begin
