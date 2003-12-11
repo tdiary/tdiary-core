9ec2787ee173a5469a42f328ffcebbee0ea5135e
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2003-12-11 08:46:48 +0000

    * skel/day.rhtml, skel/diary.rhtml: do not make link URL when access from bot.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@1707 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,13 +1,13 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.164 $
+tdiary.rb $Revision: 1.165 $
 
 Copyright (C) 2001-2003, TADA Tadashi <sho@spc.gr.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '1.5.6.20031209'
+TDIARY_VERSION = '1.5.6.20031211'
 
 require 'cgi'
 begin
