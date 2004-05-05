12a800670702be609cedfe1a6499b7e06cbd3064
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2004-05-05 07:00:14 +0000

    * release 1.5.7.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@1861 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,13 +1,13 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.186 $
+tdiary.rb $Revision: 1.187 $
 
 Copyright (C) 2001-2004, TADA Tadashi <sho@spc.gr.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '1.5.6.20040504'
+TDIARY_VERSION = '1.5.7'
 
 require 'cgi'
 begin
