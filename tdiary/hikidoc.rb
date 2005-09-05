94c169b8d6a5affe6b4056b07efaaf402f180716
kazuhiko <kazuhiko@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-09-05 03:45:50 +0000

    fix double-pre bug (reported by tamo).
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@2368 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary/hikidoc.rb
+++ tdiary/hikidoc.rb
@@ -109,13 +109,13 @@ class HikiDoc < String
 
   def parse_pre( text )
     ret = text
+    ret.gsub!( /^#{MULTI_PRE_OPEN_RE}$(.*)^#{MULTI_PRE_CLOSE_RE}$/m ) do |str|
+      "\n" + store_block( "<pre>%s</pre>" % restore_pre( $1 ) ) + "\n\n"
+    end
     ret.gsub!( /(?:^#{PRE_RE}.*\n?)+/ ) do |str|
       str.chomp!
       str.gsub!( /^#{PRE_RE}/, '' )
       "\n" + store_block( "<pre>\n%s\n</pre>" % restore_pre( str ) ) + "\n\n"
-    end
-    ret.gsub!( /^#{MULTI_PRE_OPEN_RE}$(.*)^#{MULTI_PRE_CLOSE_RE}$/m ) do |str|
-      "\n" + store_block( "<pre>%s</pre>" % restore_pre( $1 ) ) + "\n\n"
     end
     ret
   end
