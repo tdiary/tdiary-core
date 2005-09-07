96ce273290ecd90f7971f01d4131a9973f67e525
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2005-09-07 05:31:11 +0000

    fix navi_user_month more.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@2392 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/00default.rb
+++ plugin/00default.rb
@@ -1,6 +1,6 @@
 #
 # 00default.rb: default plugins 
-# $Revision: 1.88 $
+# $Revision: 1.89 $
 #
 # Copyright (C) 2001-2005, TADA Tadashi <sho@spc.gr.jp>
 # You can redistribute it and/or modify it under GPL2.
@@ -68,13 +68,13 @@ def navi_user_month
 	ym.sort!
 	now = @date.strftime( '%Y%m' )
 	return '' unless ym.index( now )
-	prev_month = ym[ym.index( now )-1]
+	prev_month = ym.index( now ) == 0 ? nil : ym[ym.index( now )-1]
 	next_month = ym[ym.index( now )+1]
 
 	result = ''
-	result << navi_item( "#{@index}#{anchor( prev_month )}", "&laquo;#{navi_prev_month}" ) if prev_month >= ym[0] and not bot?
+	result << navi_item( "#{@index}#{anchor( prev_month )}", "&laquo;#{navi_prev_month}" ) if prev_month and not bot?
 	result << navi_item( @index, navi_latest )
-	result << navi_item( "#{@index}#{anchor( next_month )}", "#{navi_next_month}&raquo;" ) if next_month <= ym[-1] and not bot?
+	result << navi_item( "#{@index}#{anchor( next_month )}", "#{navi_next_month}&raquo;" ) if next_month and not bot?
 	result
 end
 
