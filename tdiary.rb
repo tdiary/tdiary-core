2566d1a6f739eaffd4e95934b9e2bb7225fb696e
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2006-03-19 05:17:23 +0000

    * skel/show.rhtml, skel/i.show.rhtml: delete contents because these are dummy.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@2614 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,13 +1,13 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.267 $
+tdiary.rb $Revision: 1.268 $
 
 Copyright (C) 2001-2005, TADA Tadashi <sho@spc.gr.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '2.1.3.20060318'
+TDIARY_VERSION = '2.1.3.20060319'
 
 require 'cgi'
 require 'uri'
