b132ce5c8758a3f45581381b60d8692612e568e1
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2006-12-28 02:22:30 +0000

    * tdiary/filter/spam.rb: fix error on logging when no 'date' parameter (again).
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@2710 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,13 +1,13 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.289 $
+tdiary.rb $Revision: 1.290 $
 
 Copyright (C) 2001-2005, TADA Tadashi <sho@spc.gr.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '2.1.4.20061227'
+TDIARY_VERSION = '2.1.4.20061228'
 
 require 'cgi'
 require 'uri'
