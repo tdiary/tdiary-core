89be8d101ce06d8815c2d635d97db7867af85500
hsbt <hsbt@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-01-17 11:54:19 +0000

    * plugin/00default.rb: add name field for comment-form.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@2824 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/00default.rb
+++ plugin/00default.rb
@@ -1,6 +1,6 @@
 #
 # 00default.rb: default plugins 
-# $Revision: 1.103 $
+# $Revision: 1.104 $
 #
 # Copyright (C) 2001-2005, TADA Tadashi <sho@spc.gr.jp>
 # You can redistribute it and/or modify it under GPL2.
@@ -511,7 +511,7 @@ def comment_form
 		else
 			r << <<-FORM
 					<div class="caption"><a name="c">#{comment_description}</a></div>
-					<form class="comment" method="post" action="#{h @index}"><div>
+					<form class="comment" name="comment-form" method="post" action="#{h @index}"><div>
 					<input type="hidden" name="date" value="#{ @date.strftime( '%Y%m%d' )}">
 					<div class="field name">
 						#{comment_name_label}:<input class="field" name="name" value="#{h( @cgi.cookies['tdiary'][0] || '' )}">
