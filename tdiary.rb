d87cbeec3d965d152e6f42d16cfca0d166c5782f
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2007-04-22 04:39:29 +0000

    * add skel/i.category.rb for category page on mobile mode.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@3070 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary.rb
+++ tdiary.rb
@@ -1,13 +1,13 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.313 $
+tdiary.rb $Revision: 1.314 $
 
 Copyright (C) 2001-2007, TADA Tadashi <sho@spc.gr.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '2.1.4.20070410'
+TDIARY_VERSION = '2.1.4.20070422'
 
 $:.insert( 1, File::dirname( __FILE__ ) + '/misc/lib' )
 
