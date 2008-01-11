815b6ece7bd3a3384d3d77392a70ffe0d7db430f
tadatadashi <tadatadashi@7f22e88f-374d-0410-998f-c91420d97ba2>
2008-01-11 05:47:34 +0000

    * plugin/00default.rb: hide comment form when accessed by bot.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@3186 7f22e88f-374d-0410-998f-c91420d97ba2

--- plugin/00default.rb
+++ plugin/00default.rb
@@ -1,6 +1,6 @@
 #
 # 00default.rb: default plugins 
-# $Revision: 1.115 $
+# $Revision: 1.116 $
 #
 # Copyright (C) 2001-2005, TADA Tadashi <sho@spc.gr.jp>
 # You can redistribute it and/or modify it under GPL2.
@@ -515,6 +515,7 @@ end
 
 def comment_form
 	return '' unless @mode == 'day'
+	return '' if bot?
 
 	r = ''
 	unless @conf.hide_comment_form then
@@ -558,6 +559,7 @@ end
 
 def comment_form_mobile
 	return '' if @conf.hide_comment_form
+	return '' if bot?
 	if @diaries[@date.strftime('%Y%m%d')].count_comments( true ) >= @conf.comment_limit_per_day then
 		return "<HR><P>#{comment_limit_label}</P>"
 	end
--- tdiary.rb
+++ tdiary.rb
@@ -1,13 +1,13 @@
 =begin
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
-tdiary.rb $Revision: 1.328 $
+tdiary.rb $Revision: 1.329 $
 
 Copyright (C) 2001-2007, TADA Tadashi <sho@spc.gr.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '2.2.0.20080108'
+TDIARY_VERSION = '2.2.0.20080111'
 
 $:.insert( 1, File::dirname( __FILE__ ) + '/misc/lib' )
 
