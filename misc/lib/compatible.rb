2b3bcbb92b8c7341708fa694ce7282db4501e57b
hsbt <hsbt@7f22e88f-374d-0410-998f-c91420d97ba2>
2009-03-06 10:44:31 +0000

    tabify.
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@3411 7f22e88f-374d-0410-998f-c91420d97ba2

--- misc/lib/compatible.rb
+++ misc/lib/compatible.rb
@@ -20,55 +20,55 @@ Thread.start {
 }.join
 
 unless "".respond_to?('to_a')
-  class String
-    def to_a
-      [ self ]
-    end
-  end
+	class String
+		def to_a
+			[ self ]
+		end
+	end
 end
 
 unless "".respond_to?('each')
-  class String
-    alias each each_line
-  end
+	class String
+		alias each each_line
+	end
 end
 
 # Ruby1.9では String が Enumerable ではなくなった
 class String
-  def method_missing(name, *args, &block)
-    each_line.__send__(name, *args, &block)
-  end
+	def method_missing(name, *args, &block)
+		each_line.__send__(name, *args, &block)
+	end
 end
 
 
 # for Ruby1.8.X
 
 unless "".respond_to?('force_encoding')
-  class String
-    def force_encoding(encoding)
-      self
-    end
-  end
+	class String
+		def force_encoding(encoding)
+			self
+		end
+	end
 end
 
 unless "".respond_to?('bytesize')
-  class String
-    alias bytesize size
-  end
+	class String
+		alias bytesize size
+	end
 end
 
 unless "".respond_to?('ord')
-  class String
-    def ord
-      self[0]
-    end
-  end
-
-  class Integer
-    def ord
-      self
-    end
-  end
+	class String
+		def ord
+			self[0]
+		end
+	end
+	
+	class Integer
+		def ord
+			self
+		end
+	end
 end
 
 # --------------------------------------------------------
@@ -79,12 +79,12 @@ end
 # (PStore 保存前は Time だが, 保存後に String となる)
 # 暫定的に String だったら Time へ変換する
 module TDiary
-  class WikiDiary
-    def last_modified
-      if @last_modified.instance_of? String
-        @last_modified = Time.at(0)
-      end
-      @last_modified
-    end
-  end
+	class WikiDiary
+		def last_modified
+			if @last_modified.instance_of? String
+				@last_modified = Time.at(0)
+			end
+			@last_modified
+		end
+	end
 end
