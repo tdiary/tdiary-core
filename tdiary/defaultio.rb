8ed018618b2ec239135069a29c3b33368056a304
zunda <zunda@7f22e88f-374d-0410-998f-c91420d97ba2>
2009-08-15 18:21:18 +0000

    tdiary/defaultio.rb (DefaultIO#restore): modified to expose style that is not installed
    git-svn-id: https://tdiary.svn.sourceforge.net/svnroot/tdiary/trunk/core@3515 7f22e88f-374d-0410-998f-c91420d97ba2

--- tdiary/defaultio.rb
+++ tdiary/defaultio.rb
@@ -242,7 +242,7 @@ module TDiary
 						diaries[headers['Date']] = diary
 					end
 
-				rescue SyntaxError
+				rescue NoMethodError
 					raise "#{style_name} style not found."
 				rescue NameError
 					# no magic number when it is new file.
