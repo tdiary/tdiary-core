d9b8dcc0a12ae7a7079ba6654c2f79fb62861803
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-06-08 21:32:55 +0900

    categorized.

--- spec/acceptance/save_conf_default_spec.rb
+++ spec/acceptance/save_conf_default_spec.rb
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 require File.expand_path('../acceptance_helper', __FILE__)
 
-feature '設定画面の利用' do
+feature '基本設定の利用' do
 	background do
 		setup_tdiary
 	end
 
-	scenario do
-		visit '/update.rb?conf=default'
+	scenario 'サイトの情報の設定' do
+		visit '/'
+
+		click '追記'
+		click '設定'
 
 		fill_in "author_name", :with => "ただただし"
 		fill_in "html_title", :with => "ただの日記"
@@ -22,8 +25,24 @@ feature '設定画面の利用' do
 		within('title') { page.should have_content('(設定完了)') }
 		within('h1') { page.should have_content('(設定)') }
 
-		visit '/'
+		click '最新'
 
 		within('title') { page.should have_content('ただの日記') }
 	end
+
+	scenario 'ヘッダ・フッタの設定'
+
+	scenario '表示一版の設定'
+
+	scenario 'ログレベルの選択の設定'
+
+	scenario 'プラグイン選択の設定'
+
+	scenario '時差調整が保存される'
+
+	scenario 'テーマ選択が保存される'
+
+	scenario 'ツッコミが保存される'
+
+	scenario 'リンク元が保存される'
 end
---
+++ spec/acceptance/save_conf_security_spec.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+require File.expand_path('../acceptance_helper', __FILE__)
+
+feature 'spamフィルタの確認' do
+	background do
+		setup_tdiary
+	end
+
+	scenario 'おすすめフィルタの内容が保存される'
+
+	scenario 'CSRF情報が保存される'
+
+	scenario '基本設定が保存される'
+
+	scenario 'キーワードでツッコミがはじかれる'
+
+	scenario 'メールアドレスでツッコミがはじかれる'
+
+	scenario 'URLでツッコミがはじかれる'
+
+	scenario 'IPアドレスでツッコミが弾かれる'
+
+	scenario 'ツッコミの注意文が保存されて表示される'
+
+	scenario 'DNSBL設定が保存される'
+
+	scenario 'スパムフィルター選択が保存される'
+
+end
