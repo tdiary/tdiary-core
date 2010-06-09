5d13ac3c1b6a11c2dec1b17bc87df46811c8a4d9
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-06-09 14:19:25 +0900

    wip

--- spec/acceptance/save_conf_default_spec.rb
+++ spec/acceptance/save_conf_default_spec.rb
@@ -11,6 +11,7 @@ feature '基本設定の利用' do
 
 		click '追記'
 		click '設定'
+		click 'サイトの情報'
 
 		fill_in "author_name", :with => "ただただし"
 		fill_in "html_title", :with => "ただの日記"
@@ -18,23 +19,103 @@ feature '基本設定の利用' do
 		fill_in "index_page", :with => "http://www.example.com"
 		fill_in "description", :with => "ただただしによる日々の記録"
 		fill_in "icon", :with => "http://tdtds.jp/favicon.png"
-		fill_in "banner", :with => "http://sho.tdiary.net/images/banner.png"
+		# TODO banner の値が fill_in されない
+		#fill_in "banner", :with => "http://sho.tdiary.net/images/banner.png"
 
 		click_button "OK"
 
 		within('title') { page.should have_content('(設定完了)') }
-		within('h1') { page.should have_content('(設定)') }
+		save_and_open_page
 
 		click '最新'
 
+		# TODO その他の項目の反映を確認
 		within('title') { page.should have_content('ただの日記') }
+
+		click '追記'
+		click '設定'
+		click 'サイトの情報'
+
+		page.should have_field "author_name", :with => "ただただし"
+		page.should have_field "html_title", :with => "ただの日記"
+		page.should have_field "author_mail", :with => "t@tdtds.jp"
+		page.should have_field "index_page", :with => "http://www.example.com"
+		page.should have_field "description", :with => "ただただしによる日々の記録"
+		page.should have_field "icon", :with => "http://tdtds.jp/favicon.png"
+		# TODO banner の値が fill_in されない
+		# page.should have_field("banner", :with => "http://sho.tdiary.net/images/banner.png")
 	end
 
-	scenario 'ヘッダ・フッタの設定'
+	scenario 'ヘッダ・フッタの設定' do
+		visit '/'
+
+		click '追記'
+		click '設定'
+		click 'ヘッダ・フッタ'
+
+		fill_in "header", :with => <<-HEADER
+<%= navi %>
+<h1>alpha</h1>
+<div class="main">
+HEADER
+		fill_in "footer", :with => <<-FOOTER
+</div>
+<div class="sidebar">
+bravo
+</div>
+FOOTER
+
+		click_button "OK"
+
+		within('title') { page.should have_content('(設定完了)') }
+
+		click '最新'
+		
+		within('h1') { page.should have_content('alpha') }
+		within('div.sidebar') { page.should have_content('bravo')}
+
+		click '追記'
+		click '設定'
+		click 'ヘッダ・フッタ'
+
+		page.should have_field "header", :with => <<-HEADER
+<%= navi %>
+<h1>alpha</h1>
+<div class="main">
+HEADER
+		page.should have_field "footer", :with => <<-FOOTER
+</div>
+<div class="sidebar">
+bravo
+</div>
+FOOTER
+	end
 
 	scenario '表示一版の設定'
 
-	scenario 'ログレベルの選択の設定'
+	scenario 'ログレベルの選択の設定' do
+		pending
+		visit '/'
+
+		click '追記'
+		click '設定'
+		click 'ログレベル選択'
+
+		select 'DEBUG'
+
+		click_button "OK"
+
+		within('title') { page.should have_content('(設定完了)') }
+
+		click '最新'
+		
+		# TODO ログレベルの確認
+
+		click '追記'
+		click '設定'
+		click 'ログレベル選択'
+
+	end
 
 	scenario 'プラグイン選択の設定'
 
