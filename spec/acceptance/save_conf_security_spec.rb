e333c9c5166e6b1927e35f30514b221741ef5933
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-06-30 09:13:00 +0900

    added some spam filter spec.

--- spec/acceptance/save_conf_security_spec.rb
+++ spec/acceptance/save_conf_security_spec.rb
@@ -10,13 +10,184 @@ feature 'spamフィルタ設定の利用' do
 
 	scenario 'CSRF情報が保存される'
 
-	scenario '基本設定が保存される'
+	scenario 'spamと判定されたツッコミを捨てる'
 
-	scenario 'キーワードでツッコミがはじかれる'
+	scenario 'URLの数によるspam判定' do
+		append_default_diary
 
-	scenario 'メールアドレスでツッコミがはじかれる'
+		visit '/'
+		click '追記'
+		click '設定'
+		click 'spamフィルタ'
+		fill_in "spamfilter.max_uris", :with => 1
 
-	scenario 'URLでツッコミがはじかれる'
+		click_button 'OK'
+		within('title') { page.should have_content('(設定完了)') }
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "alpha"
+		fill_in "body", :with => <<-BODY
+こんにちは!こんにちは!
+http://www.example.org
+http://www.example.org
+BODY
+		click_button '投稿'
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "bravo"
+		fill_in "body", :with => <<-BODY
+こんばんは!こんばんは!
+http://www.example.org
+BODY
+		click_button '投稿'
+
+		visit "/"
+		page.should have_no_content "alpha"
+		page.should have_no_content "こんにちは!こんにちは!"
+		page.should have_content "bravo"
+		page.should have_content "こんばんは!こんばんは!"
+	end
+
+	scenario 'URLの割合によるspam判定' do
+		append_default_diary
+
+		visit '/'
+		click '追記'
+		click '設定'
+		click 'spamフィルタ'
+		fill_in "spamfilter.max_rate", :with => 50
+
+		click_button 'OK'
+		within('title') { page.should have_content('(設定完了)') }
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "alpha"
+		fill_in "body", :with => <<-BODY
+こんにちは!こんにちは!
+http://www.example.org
+http://www.example.org
+BODY
+		click_button '投稿'
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "bravo"
+		fill_in "body", :with => <<-BODY
+こんばんは!こんばんは!
+http://www.example.org
+BODY
+		click_button '投稿'
+
+		visit "/"
+		page.should have_no_content "alpha"
+		page.should have_no_content "こんにちは!こんにちは!"
+		page.should have_content "bravo"
+		page.should have_content "こんばんは!こんばんは!"
+	end
+
+	scenario 'キーワードでツッコミがはじかれる' do
+		append_default_diary
+
+		visit '/'
+		click '追記'
+		click '設定'
+		click 'spamフィルタ'
+		fill_in "spamfilter.bad_comment_patts", :with => <<-BODY
+こんにちは!
+BODY
+		click_button 'OK'
+		within('title') { page.should have_content('(設定完了)') }
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "alpha"
+		fill_in "body", :with => <<-BODY
+こんにちは!こんにちは!
+BODY
+		click_button '投稿'
+
+		visit "/"
+		page.should have_no_content "alpha"
+		page.should have_no_content "こんにちは!こんにちは!"
+	end
+
+	scenario 'メールアドレスでツッコミがはじかれる' do
+		append_default_diary
+
+		visit '/'
+		click '追記'
+		click '設定'
+		click 'spamフィルタ'
+		fill_in "spamfilter.bad_mail_patts", :with => <<-BODY
+example.com
+BODY
+		click_button 'OK'
+		within('title') { page.should have_content('(設定完了)') }
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "alpha"
+		fill_in "mail", :with => "admin@example.com"
+		fill_in "body", :with => <<-BODY
+こんにちは!こんにちは!
+BODY
+		click_button '投稿'
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "bravo"
+		fill_in "mail", :with => "t@tdtds.jp"
+		fill_in "body", :with => <<-BODY
+こんばんは!こんばんは!
+BODY
+		click_button '投稿'
+
+		visit "/"
+		page.should have_no_content "alpha"
+		page.should have_no_content "こんにちは!こんにちは!"
+		page.should have_content "bravo"
+		page.should have_content "こんばんは!こんばんは!"
+
+	end
+
+	scenario 'URLでツッコミがはじかれる' do
+		append_default_diary
+
+		visit '/'
+		click '追記'
+		click '設定'
+		click 'spamフィルタ'
+		fill_in "spamfilter.bad_uri_patts", :with => <<-BODY
+example
+BODY
+		click_button 'OK'
+		within('title') { page.should have_content('(設定完了)') }
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "alpha"
+		fill_in "body", :with => <<-BODY
+こんにちは! http://www.example.com
+BODY
+		click_button '投稿'
+
+		visit "/"
+		click 'ツッコミを入れる'
+		fill_in "name", :with => "bravo"
+		fill_in "body", :with => <<-BODY
+example こんにちは!
+BODY
+		click_button '投稿'
+
+		visit "/"
+		page.should have_no_content "alpha"
+		page.should have_no_content "こんにちは!こんにちは!"
+		page.should have_content "bravo"
+		page.should have_content "example こんにちは!"
+	end
 
 	scenario 'IPアドレスでツッコミが弾かれる'
 
