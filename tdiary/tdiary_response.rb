2eaaf3c32e1220315ad72b1651146d6c4434d4bf
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-09-30 01:26:12 +0900

    tabify

--- tdiary/tdiary_response.rb
+++ tdiary/tdiary_response.rb
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 # stolen from okkez http://github.com/hiki/hiki/blob/rack/hiki/response.rb
 module TDiary
-  if Object.const_defined?(:Rack)
-    Response = ::Rack::Response
-  else
-    class Response
-      attr_reader :body, :status, :headers
-      def initialize(body = [], status = 200, headers = {}, &block)
-        @cgi = CGI.new
-        @body = body
-        @status = status
-        @headers = headers
-        yield self if block_given?
-      end
+	if Object.const_defined?(:Rack)
+		Response = ::Rack::Response
+	else
+		class Response
+			attr_reader :body, :status, :headers
+			def initialize(body = [], status = 200, headers = {}, &block)
+				@cgi = CGI.new
+				@body = body
+				@status = status
+				@headers = headers
+				yield self if block_given?
+			end
 
-      def header
-        @cgi.header(@headers)
-      end
+			def header
+				@cgi.header(@headers)
+			end
 
-		def to_a
-			[status, headers, body]
+			def to_a
+				[status, headers, body]
+			end
 		end
-    end
-  end
+	end
 end
 
 # Local Variables:
