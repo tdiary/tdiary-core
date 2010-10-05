44a389e8083b7307b3e99b2a9505add99d5a1faa
KAKUTANI Shintaro <shintaro@kakutani.com>
2010-10-06 00:13:58 +0900

    remove TDiary::Response#header.
    this code originated from rack branch on hiki.
    tdiary doesn't use this api (so far).

--- tdiary/tdiary_response.rb
+++ tdiary/tdiary_response.rb
@@ -7,15 +7,10 @@ module TDiary
 		class Response
 			attr_reader :body, :status, :headers
 			def initialize(body = [], status = 200, headers = {}, &block)
-				@cgi = CGI.new
 				@body = body
 				@status = status
 				@headers = headers
 				yield self if block_given?
-			end
-
-			def header
-				@cgi.header(@headers)
 			end
 
 			def to_a
