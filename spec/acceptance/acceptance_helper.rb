cbe24c6b352ad720aac6463f72048642c876e257
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-04-07 22:41:20 +0900

    use original conf file for webrick test mode.

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -5,12 +5,12 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
 	fixture_conf = File.expand_path('../../fixtures/just_installed.conf', __FILE__)
-	rack_conf = File.expand_path('../../fixtures/tdiary.conf.rack', __FILE__)
+	tdiary_conf = File.expand_path("../../fixtures/tdiary.conf.#{ENV['CGI_TEST'] ? 'webrick' : 'rack'}", __FILE__)
 	work_data_dir = File.expand_path('../../../tmp/data', __FILE__)
 	work_conf = File.expand_path('../../../tdiary.conf', __FILE__)
 
 	config.before(:all) do
-		FileUtils.cp_r rack_conf, work_conf, :verbose => false
+		FileUtils.cp_r tdiary_conf, work_conf, :verbose => false
 	end
 
 	config.before(:each) do
