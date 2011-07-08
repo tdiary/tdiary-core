de1df92a84edd485e40d0273c3783b2f2470eb96
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-07-08 23:37:36 +0900

    add monkey patch for configatron

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..')).untaint
 require 'tdiary/environment'
+
+# monkey patch for configatron
+require 'yaml' unless defined?(YAML)
+YAML::ENGINE.yamler = 'syck'
+
 Bundler.require :test if defined?(Bundler)
