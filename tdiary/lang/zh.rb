1c1b6476d1a0a55a64645b5a9e5746c3e63fd664
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-08-30 21:10:56 +0900

    use String.encode when running on ruby-head

--- tdiary/lang/zh.rb
+++ tdiary/lang/zh.rb
@@ -1,27 +1,28 @@
 # -*- coding: utf-8; -*-
 #
-# tDiary language setup: (zh) $Revision: 1.4 $
+# tDiary language setup: (zh)
 #
-# Copyright (C) 2001-2005, TADA Tadashi <t@tdtds.jp>
+# Copyright (C) 2001-2011, TADA Tadashi <t@tdtds.jp>
 # You can redistribute it and/or modify it under GPL2.
 #
 
 #
 # 'html_lang' method returns String of HTML language attribute.
-# 
+#
 def html_lang
 	'zh-TW'
 end
 
 #
 # 'encoding' method returns String of HTTP or HTML charactor encoding.
-# 
+#
 def encoding
 	'UTF-8'
 end
 
 #
 # 'mobile_encoding' method returns charactor encoding in mobile mode.
+#
 def mobile_encoding
 	'UTF-8'
 end
@@ -30,10 +31,9 @@ def encoding_old
 	'Big5'
 end
 
-
 #
 # 'to_native' method converts string automatically to native encoding.
-# 
+#
 def to_native( str, charset = nil )
 	str.dup
 end
@@ -42,27 +42,31 @@ end
 # 'migrate_to_utf8' method converts string to UTF-8
 #
 def migrate_to_utf8( str )
-	require 'iconv'
-	Iconv::iconv( encoding, encoding_old, str )
+	if String.method_defined?(:encode)
+		str.encode(encoding)
+	else
+		require 'iconv'
+		Iconv::iconv( encoding, encoding_old, str )
+	end
 end
 
 #
 # 'to_mobile' method converts string automatically to mobile mode encoding.
-# 
+#
 def to_mobile( str )
 	str.dup
 end
 
 #
 # 'to_mail' method converts string automatically to E-mail encoding.
-# 
+#
 def to_mail( str )
 	str.dup
 end
 
 #
 # 'shorten' method cuts string length.
-# 
+#
 def shorten( str, length = 120 )
 	matched = str.gsub( /\n/, ' ' ).scan( /^.{0,#{length - 2}}/ )[0]
 	unless $'.empty?
