12198541b4b280be7792ee9de3d0ca0ba6066d9b
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2011-11-01 18:48:26 +0900

    remove monkey patch for using Syck

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,4 @@ $:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..')).untaint
 require 'tdiary/environment'
 require 'tdiary'
 
-# monkey patch for configatron
-require 'yaml' unless defined?(YAML)
-YAML::ENGINE.yamler = 'syck' if RUBY_VERSION > '1.9'
-
 Bundler.require :test if defined?(Bundler)
