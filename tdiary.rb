6dbb26d0b66ea9ed1e4ba5c1c761409cd9530cff
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-23 20:59:19 +0900

    bump version to 3.1.2.20120323

--- tdiary.rb
+++ tdiary.rb
@@ -3,11 +3,11 @@
 == NAME
 tDiary: the "tsukkomi-able" web diary system.
 
-Copyright (C) 2001-2011, TADA Tadashi <t@tdtds.jp>
+Copyright (C) 2001-2012, TADA Tadashi <t@tdtds.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '3.1.2.20120228'
+TDIARY_VERSION = '3.1.2.20120323'
 
 $:.unshift File.join(File::dirname(__FILE__), '/misc/lib').untaint
 Dir["#{File::dirname(__FILE__) + '/vendor/*/lib'}"].each {|dir| $:.unshift dir.untaint }
