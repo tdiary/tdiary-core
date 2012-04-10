0ea1d462aff5b495860819f03f806873d0fdddee
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-04-10 19:11:58 +0900

    split cache mechanism from IO files.

--- tdiary/io/default.rb
+++ tdiary/io/default.rb
@@ -119,6 +119,7 @@ module TDiary
 	class DefaultIO < BaseIO
 		include CommentIO
 		include RefererIO
+		include CacheIO
 
 		def initialize( tdiary )
 			@tdiary = tdiary
@@ -194,7 +195,7 @@ module TDiary
 				store_comment( cfile, diaries ) if ((dirty | force_save) & TDiaryBase::DIRTY_COMMENT) != 0
 				store_referer( rfile, diaries ) if ((dirty | force_save) & TDiaryBase::DIRTY_REFERER) != 0
 				if dirty != TDiaryBase::DIRTY_NONE or not cache then
-					store_parser_cache( date, 'default', diaries )
+					store_parser_cache(date, diaries, 'default')
 				end
 
 				if diaries.empty?
@@ -207,7 +208,7 @@ module TDiary
 					rescue Errno::ENOENT
 					end
 					begin
-						store_parser_cache( date, nil, nil)
+						store_parser_cache(date, nil, nil)
 					rescue Errno::ENOENT
 					end
 				end
@@ -235,10 +236,6 @@ module TDiary
 			calendar
 		end
 
-		def diary_factory( date, title, body, style = 'tDiary' )
-			styled_diary_factory( date, title, body, style )
-		end
-
 		def cache_path
 			path = (@tdiary.conf.cache_path || "#{@data_path}cache").untaint
 
@@ -252,108 +249,11 @@ module TDiary
 			path
 		end
 
-		def restore_cache( prefix )
-			if cache_enable?( prefix )
-				File::open( "#{cache_path}/#{cache_file( prefix )}" ) {|f| f.read } rescue nil
-			end
-		end
-
-		def store_cache( cache, prefix )
-			if cache_file( prefix )
-				File::open( "#{cache_path}/#{cache_file( prefix )}", 'w' ) do |f|
-					f.flock(File::LOCK_EX)
-					f.write( cache )
-				end
-			end
-		end
-
-		def clear_cache( target = /.*/ )
-			Dir::glob( "#{cache_path}/*.r[bh]*" ).each do |c|
-				File::delete( c.untaint ) if target =~ c
-			end
-		end
-
-		def restore_parser_cache(date, key)
-			return nil if @tdiary.ignore_parser_cache
-
-			file = date.strftime("#{cache_path}/%Y%m.parser")
-			obj = nil
-			begin
-				PStore.new(file).transaction do |cache|
-					begin
-						ver = cache.root?('version') ? cache['version'] : nil
-						if ver == TDIARY_VERSION and cache.root?(key)
-							obj = cache[key]
-						else
-							clear_cache
-						end
-						cache.abort
-					rescue PStore::Error
-					end
-				end
-			rescue
-				clear_parser_cache( date )
-			end
-			obj
-		end
-
-		def store_parser_cache(date, key, obj)
-			return nil if @tdiary.ignore_parser_cache
-
-			file = date.strftime("#{cache_path}/%Y%m.parser")
-			begin
-				PStore::new(file).transaction do |cache|
-					begin
-						cache[key] = obj
-						cache['version'] = TDIARY_VERSION
-					rescue PStore::Error
-					end
-				end
-			rescue
-				clear_parser_cache(date)
-			end
-		end
-
-		def clear_parser_cache(date)
-			file = date.strftime("#{cache_path}/%Y%m.parser")
-
-			begin
-				File.delete(file)
-				File.delete(file + '~')
-			rescue
-			end
-
-			nil
+		def diary_factory( date, title, body, style = 'tDiary' )
+			styled_diary_factory( date, title, body, style )
 		end
 
 	private
-
-		def cache_file( prefix )
-			if @tdiary.is_a?(TDiaryMonth)
-				"#{prefix}#{@tdiary.rhtml.sub( /month/, @tdiary.date.strftime( '%Y%m' ) ).sub( /\.rhtml$/, '.rb' )}"
-			elsif @tdiary.is_a?(TDiaryLatest)
-				if @tdiary.cgi.params['date'][0] then
-					nil
-				else
-					"#{prefix}#{@tdiary.rhtml.sub( /\.rhtml$/, '.rb' )}"
-				end
-			else
-				nil
-			end
-		end
-
-		def cache_exists?( prefix )
-			cache_file( prefix ) && FileTest::file?( "#{cache_path}/#{cache_file( prefix )}" )
-		end
-
-		def cache_enable?( prefix )
-			if @tdiary.is_a?(TDiaryView)
-				cache_exists?( prefix ) && (File::mtime( "#{cache_path}/#{cache_file( prefix )}" ) > @tdiary.last_modified)
-			else
-				cache_exists?( prefix )
-			end
-		end
-
 		def restore( fh, diaries )
 			begin
 				fh.seek( 0 )
--- tdiary/io/heroku.rb
+++ tdiary/io/heroku.rb
@@ -15,7 +15,6 @@
 # You can distribute this under GPL.
 require 'tdiary/io/base'
 require 'sequel'
-require 'dalli'
 
 module TDiary
   module CommentIO
@@ -63,6 +62,7 @@ module TDiary
   class HerokuIO < BaseIO
     include CommentIO
     include RefererIO
+    include CacheIO
 
     def initialize(tdiary)
       @tdiary = tdiary
@@ -112,33 +112,6 @@ module TDiary
       store_parser_cache(date, diaries) if dirty || !cache
     end
 
-    def cache_path
-      Dir.tmpdir
-    end
-
-    def restore_cache(prefix)
-      if key = cache_key(prefix)
-        memcache.get(key)
-      end
-    end
-
-    def store_cache(cache, prefix)
-      if key = cache_key(prefix)
-        memcache.set(key, cache)
-      end
-    end
-
-    def clear_cache(target = :all)
-      if target == :all
-        memcache.flush
-      else
-        ym = target.to_s.scan(/\d{4}\d{2}/)[0]
-        ['latest.rb', 'i.latest.rb', "#{ym}.rb", "i.#{ym}.rb"].each do |key|
-          memcache.delete(key)
-        end
-      end
-    end
-
     def calendar
       calendar = Hash.new{|hash, key| hash[key] = []}
       Sequel.connect(@tdiary.conf.database_url || ENV['DATABASE_URL']) do |db|
@@ -149,37 +122,15 @@ module TDiary
       calendar
     end
 
+    def cache_path
+      Dir.tmpdir
+    end
+
     def diary_factory(date, title, body, style = 'tDiary')
       styled_diary_factory(date, title, body, style)
     end
 
   private
-
-    def restore_parser_cache(date)
-      memcache.get(date.strftime("%Y%m.parser"))
-    end
-
-    def store_parser_cache(date, obj)
-      memcache.set(date.strftime("%Y%m.parser"), obj)
-    end
-
-    def clear_parser_cache(date)
-      memcache.flush
-    end
-
-    def cache_key(prefix)
-      if @tdiary.is_a?(TDiaryMonth)
-        "#{prefix}#{@tdiary.rhtml.sub( /month/, @tdiary.date.strftime( '%Y%m' ) ).sub( /\.rhtml$/, '.rb' )}"
-      elsif @tdiary.is_a?(TDiaryLatest)
-        if @tdiary.cgi.params['date'][0]
-          nil
-        else
-          "#{prefix}#{@tdiary.rhtml.sub( /\.rhtml$/, '.rb' )}"
-        end
-      else
-        nil
-      end
-    end
 
     def restore(date, diaries, month = true)
       Sequel.connect(@tdiary.conf.database_url || ENV['DATABASE_URL']) do |db|
@@ -218,10 +169,6 @@ module TDiary
           end
         end
       end
-    end
-
-    def memcache
-      @_client ||= Dalli::Client.new
     end
   end
 end
