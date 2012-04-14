4dbf0dc88ba6d3653d72a1b0cd592f14ae5926a5
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-04-14 21:59:11 +0900

    bump version to 3.1.2.20120414

--- tdiary.rb
+++ tdiary.rb
@@ -7,7 +7,7 @@ Copyright (C) 2001-2012, TADA Tadashi <t@tdtds.jp>
 You can redistribute it and/or modify it under GPL2.
 =end
 
-TDIARY_VERSION = '3.1.2.20120328'
+TDIARY_VERSION = '3.1.2.20120414'
 
 $:.unshift File.join(File::dirname(__FILE__), '/misc/lib').untaint
 Dir["#{File::dirname(__FILE__) + '/vendor/*/lib'}"].each {|dir| $:.unshift dir.untaint }
