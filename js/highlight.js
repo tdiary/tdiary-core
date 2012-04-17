db5197a227e2b77834db10a247c29ee0d866b33c
TADA Tadashi <t@tdtds.jp>
2012-04-17 13:10:36 +0900

    set section title function back.

--- js/highlight.js
+++ js/highlight.js
@@ -11,7 +11,14 @@ $(function() {
   function highlight(anchor) {
     // clear current highlight
     $(".highlight").removeClass("highlight");
-    $(anchor).parent().addClass("highlight");
+
+    // change document title
+    var target = $(anchor).parent();
+    if (target.filter('h3').size() > 0) {
+      document.title = target.children("a").attr("title") + " - " + $tDiary.title;
+    }
+
+    target.addClass("highlight");
   }
 
   // bind click event to anchors
--- misc/plugin/highlight.rb
+++ misc/plugin/highlight.rb
@@ -9,6 +9,8 @@
 
 if @mode == 'day' and not bot? then
 	enable_js('highlight.js')
+	title = (@conf.html_title.gsub(/\\/, '\\\\\\') || '').gsub(/"/, '\\"')
+	add_js_setting('$tDiary.title', %Q|"#{title}(#{@date.strftime('%Y-%m-%d')})"|)
 end
 
 # Local Variables:
