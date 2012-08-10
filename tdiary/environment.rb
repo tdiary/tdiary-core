bd38c45035c4cbda8f55610bfa0ae01b30e5f6d3
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-08-10 09:41:20 +0900

    added workaround for 1.8

--- tdiary/environment.rb
+++ tdiary/environment.rb
@@ -7,5 +7,9 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 if defined?(Bundler)
-  Bundler.require :default, ENV['RACK_ENV'] || ''
+  # workaround: ruby-1.8 is not accept to ''.intern
+  env = [:default]
+  env += ENV['RACK_ENV'] if ENV['RACK_ENV']
+
+  Bundler.require *env
 end
