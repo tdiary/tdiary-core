8c03e32ca2da1fae4ca39da4e2690056adb58fe6
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-08-12 11:40:09 +0900

    fixed rack environment options, RACK_ENV is symbol.

--- tdiary/environment.rb
+++ tdiary/environment.rb
@@ -9,7 +9,7 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 if defined?(Bundler)
   # workaround: ruby-1.8 is not accept to ''.intern
   env = [:default]
-  env += ENV['RACK_ENV'] if ENV['RACK_ENV']
+  env << ENV['RACK_ENV'].intern if ENV['RACK_ENV']
 
   Bundler.require *env
 end
