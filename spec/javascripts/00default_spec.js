8a719f7406523bf11105b20e49f409f1157fb145
MATSUOKA Kohei <kohei@machu.jp>
2012-08-31 20:37:09 +0900

    add javascript spec

--- spec/javascripts/00default_spec.js
+++ spec/javascripts/00default_spec.js
@@ -7,3 +7,20 @@ describe("$tDiary", function() {
     expect($tDiary.plugin).toEqual(jasmine.any(Object));
   });
 });
+
+describe("$", function() {
+  describe("#makePluginTag", function() {
+    describe("when Wiki style", function() {
+      beforeEach(function() {
+        $tDiary.style = 'wiki';
+      });
+
+      it('should create wiki style plugin tag', function() {
+        var tag = $.makePluginTag("plugin_name", ["param1", "param2"]);
+        expect(tag).toEqual('{{plugin_name "param1", "param2"}}');
+      });
+    });
+  });
+  describe("#insertAtCaret", function() {
+  });
+});
