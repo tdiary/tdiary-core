1dd5c51a9cc302fba5fafe970ce43b300789940d
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-10-07 12:11:44 +0900

    added style

--- misc/plugin/category_autocomplete.rb
+++ misc/plugin/category_autocomplete.rb
@@ -13,11 +13,16 @@ return unless /\A(?:form|preview|append|edit|update)\z/ =~ @mode
 enable_js('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js')
 enable_js('caretposition.js')
 enable_js('category_autocomplete.js')
+
 if @categories.size > 0
    categories_json = @categories.map{ |c| "\"#{c}\"" }
    add_js_setting('$tDiary.plugin.category_autocomplete')
    add_js_setting('$tDiary.plugin.category_autocomplete.candidates',
                   "[#{categories_json.join(",")}]")
+end
+
+add_header_proc do
+  %Q|<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css"/>|
 end
 
 # Local Variables:
