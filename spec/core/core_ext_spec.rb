8f53a5d043696c1545816c42517a7f9e2b18324c
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-01-13 10:44:32 +0900

    tabify

--- spec/core/core_ext_spec.rb
+++ spec/core/core_ext_spec.rb
@@ -4,19 +4,19 @@ require 'spec_helper'
 require 'tdiary/core_ext'
 
 describe "core extension library" do
-  describe 'String#make_link' do
-    it 'エスケープされて出力される' do
-      "<\"alice&bob\">".make_link.should eq "&lt;&quot;alice&amp;bob&quot;&gt;"
-    end
+	describe 'String#make_link' do
+		it 'エスケープされて出力される' do
+			"<\"alice&bob\">".make_link.should eq "&lt;&quot;alice&amp;bob&quot;&gt;"
+		end
 
-    context 'httpを含む場合' do
-      it { "http://example.com".make_link.should eq "<a href=\"http://example.com\">http://example.com</a>" }
-    end
+		context 'httpを含む場合' do
+			it { "http://example.com".make_link.should eq "<a href=\"http://example.com\">http://example.com</a>" }
+		end
 
-    context 'メールアドレスのようなものを含む場合' do
-      it { "shibata.hiroshi@gmail.com".make_link.should eq "<a href=\"mailto:shibata.hiroshi@gmail.com\">shibata.hiroshi@gmail.com</a>" }
-    end
-  end
+		context 'メールアドレスのようなものを含む場合' do
+			it { "shibata.hiroshi@gmail.com".make_link.should eq "<a href=\"mailto:shibata.hiroshi@gmail.com\">shibata.hiroshi@gmail.com</a>" }
+		end
+	end
 end
 
 # Local Variables:
