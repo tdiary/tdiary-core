b7574706df541230346cd4efa8130ed87cb09e74
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-01-22 09:39:46 +0900

    changelog

--- misc/plugin/recent_comment3.rb
+++ misc/plugin/recent_comment3.rb
@@ -21,6 +21,7 @@ def migrate_old_data
 	if File.exists?("#{@cache_path}/recent_comments") && !File.exists?("{#{@conf.data_path}/recent_comments")
 		FileUtils.mv( "#{@cache_path}/recent_comments", "#{@conf.data_path}/recent_comments" )
 	end
+	# workaround for "/foo//bar" doesn't equal "/foo/bar"
 	if @conf['recent_comment3.cache'] &&
 			Pathname(@conf['recent_comment3.cache']).cleanpath == Pathname("#{@cache_path}/recent_comments").cleanpath
 		@conf['recent_comment3.cache'] = "#{@conf.data_path}/recent_comments"
