721dca96ec538e4067f256dc0050253cfbdb68c1
MATSUOKA Kohei <kohei@machu.jp>
2013-02-03 20:03:43 +0900

    add spec for section_permalink anchor plugin

--- spec/core/rack/html_anchor_spec.rb
+++ spec/core/rack/html_anchor_spec.rb
@@ -15,13 +15,28 @@ describe TDiary::Rack::HtmlAnchor do
 			last_request.query_string.should == ''
 		end
 
+		it 'should remove the file name from PATH_INFO' do
+			get '/20120501.html'
+			last_request.env['PATH_INFO'].should == '/'
+			get '/diary/20120501.html'
+			last_request.env['PATH_INFO'].should == '/diary/'
+		end
+
 		it 'should add date query' do
+			get '/diary/0501.html'
+			last_request.params['date'].should == "0501"
 			get '/0501.html'
 			last_request.params['date'].should == "0501"
 			get '/201205.html'
 			last_request.params['date'].should == "201205"
 			get '/20120501.html'
 			last_request.params['date'].should == "20120501"
+		end
+
+		it 'should add date query when using section_permalink_anchor plugin' do
+			get '/20120501p01.html'
+			last_request.params['date'].should == "20120501"
+			last_request.params['p'].should == "01"
 		end
 
 		it 'should replace date query' do
@@ -34,6 +49,9 @@ describe TDiary::Rack::HtmlAnchor do
 			last_request.params['date'].should == "20120501"
 			get '/index.rb?date=20120501'
 			last_request.params['date'].should == "20120501"
+			get '/index.rb?date=20120501&p=01'
+			last_request.params['date'].should == "20120501"
+			last_request.params['p'].should == "01"
 		end
 	end
 end
