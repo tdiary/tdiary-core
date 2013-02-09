468fc22cf04e069fb2a41f333b3691cca2bb53e7
MATSUOKA Kohei <kohei@machu.jp>
2013-02-09 21:58:42 +0900

    post comment with ajax

---
+++ js/comment_ajax.js
@@ -0,0 +1,22 @@
+/**
+ * comment_ajax.js:
+ *
+ * Copyright (C) 2013 by MATSUOKA Kohei <kohei@machu.jp>
+ * You can distribute it under GPL.
+ */
+
+$(function() {
+	var form = $('form.comment');
+	$('<input type="hidden">')
+		.attr('name', 'comment')
+		.appendTo(form);
+	form.submit(function(e) {
+		e.preventDefault();
+		$(':submit', form).attr('disabled', 'disabled');
+		$.post(form.attr('action'), form.serialize(), function(data) {
+			form[0].reset();
+			$(':submit', form).removeAttr('disabled');
+			console.log(data);
+		}, 'html');
+	});
+});
---
+++ misc/plugin/comment_ajax.rb
@@ -0,0 +1,11 @@
+if @mode == 'day' and not bot? then
+	enable_js('comment_ajax.js')
+end
+
+# Local Variables:
+# mode: ruby
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# End:
+# vi: ts=3 sw=3
