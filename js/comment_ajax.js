cedb702337872915aa25e907db379f0d843fbdc3
MATSUOKA Kohei <kohei@machu.jp>
2013-02-10 07:38:26 +0900

    support AuthPagerize/AutoPatchWork

--- js/comment_ajax.js
+++ js/comment_ajax.js
@@ -6,20 +6,35 @@
  */
 
 $(function() {
-	var form = $('form.comment');
-	$('<input type="hidden">')
+	function comment_ajax(target) {
+		var form = $('form.comment', target);
+		var comment = $('div.comment', target);
+		$('<input type="hidden">')
 		.attr('name', 'comment')
 		.appendTo(form);
-	form.submit(function(e) {
-		e.preventDefault();
-		$(':submit', form).attr('disabled', 'disabled');
-		$.post(form.attr('action'), form.serialize(), function(data) {
-			form[0].reset();
-			$(':submit', form).removeAttr('disabled');
-			var comment = $('div.comment');
-			// $(data) is a diary HTML of the day
-			comment.after($('div.comment', $(data)));
-			comment.remove();
-		}, 'html');
+		form.submit(function(e) {
+			e.preventDefault();
+			$(':submit', form).attr('disabled', 'disabled');
+			$.post(form.attr('action'), form.serialize(), function(data) {
+				form[0].reset();
+				$(':submit', form).removeAttr('disabled');
+				// $(data) is a diary HTML of the day
+				comment.after($('div.comment', $(data)));
+				comment.remove();
+				}, 'html');
+		});
+	}
+
+	$(window).bind('AutoPagerize_DOMNodeInserted', function(event) {
+		comment_ajax(event.target);
 	});
+
+	// does not support IE8 or earlier
+	if (window.addEventListener) {
+		window.addEventListener('AutoPatchWork.DOMNodeInserted', function(event) {
+			comment_ajax(event.target);
+		}, false);
+	}
+
+	comment_ajax(document);
 });
