b5909e4a2a5e6c66c88ee519f86a134da3cb6004
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-02-19 09:46:57 +0900

    oops

--- misc/plugin/xmlrpc/xmlrpc.rb
+++ misc/plugin/xmlrpc/xmlrpc.rb
@@ -25,7 +25,7 @@ else
   server = XMLRPC::CGIServer.new
 end
 
-server.class.class_eval { include TDiary::ViewHelper }
+include TDiary::ViewHelper
 
 server.add_handler('blogger.newPost') do |appkey, blogid, username, password, content, publish|
   @cgi = CGI::new
