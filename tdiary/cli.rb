e0505b4ddd25a61053ec778f35dea5ec7df27d43
MATSUOKA Kohei <kohei@machu.jp>
2013-05-02 01:16:43 +0900

    implement tdiary new command

--- tdiary/cli.rb
+++ tdiary/cli.rb
@@ -6,6 +6,26 @@ module TDiary
 	class CLI < Thor
 		include Thor::Actions
 
+		def self.source_root
+			TDiary.root
+		end
+
+		desc "new DIR_NAME", "Create a new tDiary directory"
+		def new(name)
+			target = File.join(Dir.pwd, name)
+			empty_directory(target)
+			empty_directory(File.join(target, 'public'))
+			%w(README.md Gemfile config.ru tdiary.conf.beginner
+				tdiary.conf.sample tdiary.conf.sample-en).each do |file|
+				copy_file(file, File.join(target, file))
+			end
+			copy_file('tdiary.conf.beginner', File.join(target, 'tdiary.conf'))
+			directory('doc', File.join(target, 'doc'))
+			inside(target) do
+				run('bundle install --without test development')
+			end
+		end
+
 		desc "version", "Prints the tDiary's version information"
 		def version
 			say "tdiary #{TDiary::VERSION}"
