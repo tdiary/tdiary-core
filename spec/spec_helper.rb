cd50cc04aa6d5b53adce327b8e18b959592993a3
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-05-05 01:56:41 +0900

    remove coverage tool for 1.8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,30 +5,14 @@ ENV['RACK_ENV'] = "test"
 
 require 'tdiary/environment'
 
-if ENV['COVERAGE'] == 'simplecov'
-	require 'simplecov'
-	require 'simplecov-rcov'
-	class SimpleCov::Formatter::MergedFormatter
-		def format(result)
-			SimpleCov::Formatter::HTMLFormatter.new.format(result)
-			SimpleCov::Formatter::RcovFormatter.new.format(result)
-		end
-	end
-	SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
-	SimpleCov.start do
-		add_filter '/spec/'
-		add_filter '/vendor/'
-	end
-elsif RUBY_VERSION > '1.9'
-	require 'simplecov'
-	require 'coveralls'
+require 'simplecov'
+require 'coveralls'
 
-	SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-	SimpleCov.start do
-		add_filter '/spec/'
-		add_filter '/test/'
-		add_filter '/vendor/'
-	end
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+	add_filter '/spec/'
+	add_filter '/test/'
+	add_filter '/vendor/'
 end
 
 require 'tdiary'
