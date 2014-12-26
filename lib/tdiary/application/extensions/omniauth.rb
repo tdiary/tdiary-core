e1dbb0569777e9c90db9973d87e319b847700865
MATSUOKA Kohei <kohei@machu.jp>
2014-12-27 07:50:52 +0900

    remove github authentication

--- lib/tdiary/application/extensions/omniauth.rb
+++ lib/tdiary/application/extensions/omniauth.rb
@@ -8,7 +8,6 @@ TDiary::Application.configure do
 		use OmniAuth::Builder do
 			configure {|conf| conf.path_prefix = "/auth" }
 			provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-			provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 		end
 
 		map('/auth') do
@@ -18,6 +17,6 @@ TDiary::Application.configure do
 
 	config.authenticate TDiary::Rack::Auth::OmniAuth, :twitter do |auth|
 		# TODO: an user can setting
-		auth.info.nickname == ENV['TWITTER_NAME'] || ENV['GITHUB_NAME']
+		auth.info.nickname == ENV['TWITTER_NAME']
 	end
 end
