e4be4831be7366e40f292879cfed9b25c76a0f25
北䑓如法 <algebraicallyClosedField@gmail.com>
2017-04-03 19:38:00 +0900

    fix multiple og:image meta tags

--- lib/tdiary/plugin/00default.rb
+++ lib/tdiary/plugin/00default.rb
@@ -324,14 +324,14 @@ end
 
 def default_ogp
 	if @conf.options2['sp.selected'] && @conf.options2['sp.selected'].include?('ogp.rb')
-		if defined?(@conf.banner)
+		if !defined?(@conf.banner) && @conf.banner != ''
 			%Q[<meta content="#{base_url}images/ogimage.png" property="og:image">]
 		end
 	else
 		uri = @conf.index.dup
 		uri[0, 0] = base_url if %r|^https?://|i !~ @conf.index
 		uri.gsub!( %r|/\./|, '/' )
-		image = @conf.banner.nil? ? File.join(uri, "#{theme_url}/ogimage.png") : @conf.banner
+		image = (@conf.banner.nil? || @conf.banner == '') ? File.join(uri, "#{theme_url}/ogimage.png") : @conf.banner
 		ogp = {
 			'og:title' => title_tag.gsub(/<[^>]*>/, ""),
 			'og:image' => (h image),
