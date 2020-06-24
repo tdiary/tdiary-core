5b62ff373512f7bac22524a18eb305158646844d
Tada, Tadashi <t@tdtds.jp>
2020-06-24 05:24:01 +0000

    save some settings in devcontainer
    * bundler settings
    * ssh known_hosts
    
    Signed-off-by: Tada, Tadashi <t@tdtds.jp>

--- .devcontainer/setup-app.sh
+++ .devcontainer/setup-app.sh
@@ -2,8 +2,11 @@
 if [ ! -f tdiary.conf ]; then
 	cp tdiary.conf.beginner tdiary.conf
 fi
-bundle --path=vendor/bundle --jobs=4 --retry=3
+bundle config set path vendor/bundle
+bundle config set with development:test
+bundle install --jobs=4 --retry=3
 bundle exec rake assets:copy
 if [ ! -f .htpasswd ]; then
 	bundle exec bin/tdiary htpasswd
 fi
+git fetch
